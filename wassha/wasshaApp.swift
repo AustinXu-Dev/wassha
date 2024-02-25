30ac8a09615f321d22140f6f1c8f8b6d991a0d04
Skypoppi <austin.xu.me@gmail.com>
2024-02-25 19:17:26 +0700

    Initial Commit

---
+++ wassha/wasshaApp.swift
@@ -0,0 +1,17 @@
+//
+//  wasshaApp.swift
+//  wassha
+//
+//  Created by Austin Xu on 2024/2/25.
+//
+
+import SwiftUI
+
+@main
+struct wasshaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
